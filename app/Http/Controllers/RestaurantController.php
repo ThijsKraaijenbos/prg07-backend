6f9f625faad418593570e49ffabc97e5eaa2ae77
Thijs Kraaijenbos <47988622+ThijsKraaijenbos@users.noreply.github.com>
2025-07-03 15:27:46 +0200

    Add create options for restaurants and tags

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -14,6 +14,38 @@ class RestaurantController extends Controller
         return response()->json(RestaurantListResource::collection($restaurants));
     }
 
+    public function store(Request $request) {
+        $validated = $request->validate([
+            "name" => "required",
+            "description" => "required",
+            "img_url" => "required",
+            "star_count" => "required",
+            "address" => "required",
+            "latitude" => "required",
+            "longitude" => "required",
+            "tags"    => "required|array|min:1",
+            "tags.*"  => "required|integer|distinct|exists:tags,id",
+        ]);
+
+        try {
+            $restaurant = new Restaurant();
+            $restaurant->name = $validated['name'];
+            $restaurant->description = $validated['description'];
+            $restaurant->img_url = $validated['img_url'];
+            $restaurant->star_count = $validated['star_count'];
+            $restaurant->address = $validated['address'];
+            $restaurant->latitude = $validated['latitude'];
+            $restaurant->longitude = $validated['longitude'];
+            $restaurant->save();
+            $restaurant->tags()->attach($validated['tags']);
+
+            $restaurant->load("tags");
+            return response()->json($restaurant);
+        } catch (\Exception $e) {
+            return response()->json($e);
+        }
+    }
+
     public function show(Request $request, string $id) {
         $restaurant = Restaurant::where("id", $id)->first();
         return response()->json(new RestaurantResource($restaurant));
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/restaurants", [RestaurantController::class, "index"]);
 Route::get("/restaurants/{id}", [RestaurantController::class, "show"]);
+Route::post("/restaurants", [RestaurantController::class, "store"]);
+Route::post("/tags", [TagController::class, "store"]);
